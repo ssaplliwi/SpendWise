e6cc9d825c23fbb2188fb1ddd64b865c17e527dc
Hà Hoàng Quân <161729769+TysonHaH@users.noreply.github.com>
2026-03-31 23:24:53 +0700

    login

--- app/src/main/java/com/example/spendwise/ui/auth/LoginActivity.java
+++ app/src/main/java/com/example/spendwise/ui/auth/LoginActivity.java
@@ -1,4 +1,17 @@
 package com.example.spendwise.ui.auth;
 
-public class LoginActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.spendwise.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+    }
 }
