b48ad7953b7a970438fbd12620a146c85312f4aa
DTS-D2X <hainam26112003@gmail.com>
2026-03-31 22:24:05 +0700

    edit SpendingRepository.java

--- app/src/main/java/com/example/spendwise/repository/SpendingRepository.java
+++ app/src/main/java/com/example/spendwise/repository/SpendingRepository.java
@@ -1,4 +1,35 @@
 package com.example.spendwise.repository;
 
+import android.content.Context;
+import com.example.spendwise.database.SQLiteHelper;
+import com.example.spendwise.model.Transaction;
+import java.util.List;
+
 public class SpendingRepository {
+    private SQLiteHelper db;
+
+    public SpendingRepository(Context context) {
+        db = new SQLiteHelper(context);
+    }
+
+    public long insert(Transaction t) {
+        return db.addTransaction(t);
+    }
+
+    public List<Transaction> getAll() {
+        return db.getAll();
+    }
+
+    public int delete(int id) {
+        return db.delete(id);
+    }
+
+    public double getTotalBalance() {
+        double total = 0;
+        List<Transaction> list = db.getAll();
+        for (Transaction t : list) {
+            total += t.getPrice();
+        }
+        return total;
+    }
 }
